9e5e0854b2e64aee886eaa466ffccaad41b90fca
Michael Katica <michael.katica@gmail.com>
2024-04-18 01:23:37 -0400

    read into correct type

--- services/webservices/subtitle_webservice.go
+++ services/webservices/subtitle_webservice.go
@@ -232,8 +232,8 @@ func New(bindaddr string, connectionstring string) *SubtitleWebservice {
 	r.POST("/extract/bulk", svc.ExtractSubtitleBulkAPI())
 	r.GET("/results", func (c *gin.Context) {
 		cdeadline := time.Now().Add(60 * time.Second)
-		var res SubtitleExtractResult
-		err := svc.DbEngine.NewSelect().Model(res).Order("id DESC").Limit(20).Scan(context.WithDeadline(context.Background(), cdeadline))
+		var res []SubtitleExtractResult
+		err := svc.DbEngine.NewSelect().Model(&res).Order("id DESC").Limit(20).Scan(context.WithDeadline(context.Background(), cdeadline))
 		if err != nil {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
