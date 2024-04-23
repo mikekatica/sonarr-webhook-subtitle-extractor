0ed8b8ba78409cc9cd14580429c6321403785e85
Michael Katica <michael.katica@gmail.com>
2024-04-23 14:22:52 -0400

    fix typo

--- subtitleparser/subtitle_parser.go
+++ subtitleparser/subtitle_parser.go
@@ -141,7 +141,7 @@ func ExtractSubtitleInfo(filepath string) (map[int64]SubtitleTrack, error) {
 		Subtitles: make(map[int64]SubtitleTrack),
 	}
 	glog.V(4).Info("Parsing sub tracks from mkv")
-	err := mkvparse.ParseSections(file, &h, mkvparse.TrackEntryElement, mkvParse.TracksElement)
+	err := mkvparse.ParseSections(file, &h, mkvparse.TrackEntryElement, mkvparse.TracksElement)
 	if err != nil {
 		glog.Error(err)
 	}
