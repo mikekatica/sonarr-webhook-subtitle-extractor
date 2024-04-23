cce0c0fcb85ac389230582a8b84d6d5e77b6569a
Michael Katica <michael.katica@gmail.com>
2024-04-23 13:52:30 -0400

    try parse without section filter

--- subtitleparser/subtitle_parser.go
+++ subtitleparser/subtitle_parser.go
@@ -142,7 +142,7 @@ func ExtractSubtitleInfo(filepath string) (map[int64]SubtitleTrack, error) {
 		Subtitles: make(map[int64]SubtitleTrack),
 	}
 	glog.V(4).Info("Parsing sub tracks from mkv")
-	err := mkvparse.ParseSections(file, &h, mkvparse.TracksElement)
+	err := mkvparse.Parse(file, &h)
 	if err != nil {
 		glog.Error(err)
 	}
